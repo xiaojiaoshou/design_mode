d20ee879f2e5c5ed0f60feaeed48216393fe46c7
xiaojiaoshou <963459511@qq.com>
2020-05-06 11:43:58 +0800

    项目初始化

---
+++ src/main/java/com/example/design_mode/DesignModeApplication.java
@@ -0,0 +1,12 @@
+package com.example.design_mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/design_mode/DesignModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.design_mode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
